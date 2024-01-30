731cc20e085bde0dd9e1d54de366b9eeef62159f
nik-1207 <nmknikhilkashyap@gmail.com>
2024-01-30 20:01:08 +0530

    refactor: pretty

--- src/editor.rs
+++ src/editor.rs
@@ -7,23 +7,22 @@ pub struct Editor {}
 
 impl Editor {
     pub fn run(self) {
-        let _stdout = stdout().into_raw_mode().unwrap(); // entring raw mode for terminal i.e it will not wait terminal to press 'enter' key to read the input.
+        let _stdout = stdout().into_raw_mode().unwrap(); // entering raw mode for terminal i.e it will not wait terminal to press 'enter' key to read the input.
 
-        loop{
-            if let Err(error)=self.process_key_press(){
+        loop {
+            if let Err(error) = self.process_key_press() {
                 die(&error);
             }
         }
     }
 
-    fn process_key_press(&self)->Result<(),std::io::Error>{
-        let key= read_key()?;
+    fn process_key_press(&self) -> Result<(), std::io::Error> {
+        let key = read_key()?;
         match key {
-            Key::Ctrl('c')=>panic!("exiting..."),
-            _ => ()
+            Key::Ctrl('c') => panic!("exiting..."),
+            _ => (),
         }
         Ok(())
-
     }
 
     pub fn default() -> Self {
