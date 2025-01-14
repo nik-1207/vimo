ea93770343d2ab3a77d997c76829b15868235b2a
nik-1207 <nmknikhilkashyap@gmail.com>
2025-01-14 04:45:16 +0000

    add logger

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 // Lint rule
 #![warn(clippy::all, clippy::pedantic)]
-
 mod editor;
+use log4rs;
 use editor::Editor;
+use log::info;
+
 
 fn main() {
+    log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+    info!("Stating application");
     Editor::default().run();
 }
