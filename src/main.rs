0097c3d3bdbfea664a40acca3f84943e02117c3a
nik-1207 <nmknikhilkashyap@gmail.com>
2025-01-14 04:45:38 +0000

    refactor: minor

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 // Lint rule
 #![warn(clippy::all, clippy::pedantic)]
 mod editor;
-use log4rs;
 use editor::Editor;
 use log::info;
+use log4rs::config::Deserializers;
 
 
 fn main() {
-    log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+    log4rs::init_file("log4rs.yml", Deserializers::default()).unwrap();
     info!("Stating application");
     Editor::default().run();
 }
